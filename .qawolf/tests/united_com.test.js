9ad4d9ab3e6b4ab8fdcc7483125fc24aa0f62192
Jon Perl <perl.jonathan@gmail.com>
2019-12-02 11:01:50 -0500

    update to v0.6.0-alpha.0

--- .qawolf/tests/united_com.test.js
+++ .qawolf/tests/united_com.test.js
@@ -1,53 +1,49 @@
 describe('united_com', () => {
-    it('can type into "from*" input', async () => {
-      await type(steps[0], values[0]);
-    });
-  
-    it('can Tab', async () => {
-      await type(steps[1], values[1]);
-    });
-  
-    it('can type into "to*" input', async () => {
-      await type(steps[2], values[2]);
-    });
-  
-    it('can Tab', async () => {
-      await type(steps[3], values[3]);
-    });
-  
-    it('can Tab', async () => {
-      await type(steps[4], values[4]);
-    });
-  
-    it('can type into "DepartDate" input', async () => {
-      await type(steps[5], values[5]);
-    });
-  
-    it('can Tab', async () => {
-      await type(steps[6], values[6]);
-    });
-  
-    it('can type into "ReturnDate" input', async () => {
-      await type(steps[7], values[7]);
-    });
-  
-    it('can Tab', async () => {
-      await type(steps[8], values[8]);
-    });
-  
-    it('can click "find flights" span', async () => {
-      await click(steps[9]);
-    });
-  
-    it('can click "sort flights by: sort flights by dropdow..." div', async () => {
-      await click(steps[10]);
-    });
-  
-    it('can click "afternoon departure" link', async () => {
-      await click(steps[11]);
-    });
-  
-    it('can scroll', async () => {
-      await scroll(steps[12], values[12]);
-    });
+  it('can type into "From*" input', async () => {
+    await type(steps[0], values[0]);
   });
+
+  it('can click div', async () => {
+    await click(steps[1]);
+  });
+
+  it('can type into "To*" input', async () => {
+    await type(steps[2], values[2]);
+  });
+
+  it('can click div', async () => {
+    await click(steps[3]);
+  });
+
+  it('can type into "DepartDate" input', async () => {
+    await type(steps[4], values[4]);
+  });
+
+  it('can Tab', async () => {
+    await type(steps[5], values[5]);
+  });
+
+  it('can type into "ReturnDate" input', async () => {
+    await type(steps[6], values[6]);
+  });
+
+  it('can click div', async () => {
+    await click(steps[7]);
+  });
+
+  it('can click "Find flights" button', async () => {
+    await click(steps[8]);
+  });
+
+  it('can click "Sort flights by: &#10;Sort flights by dr..." div', async () => {
+    await click(steps[9]);
+  });
+
+  it('can click "Afternoon departure" link', async () => {
+    await click(steps[10]);
+  });
+
+  it('can scroll', async () => {
+    await scroll(steps[11], values[11]);
+  });
+});
